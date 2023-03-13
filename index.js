253e5fc5e2149e163f0cbac90538ee1b2e505a2e
Paul Nadola Oyinga <nadolapaul@gmail.com>
2023-03-13 20:13:34 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Write your code here
+/*function arithmeticMultiplication(num1, num2){
+    const multiply = num1*num2;
+    return multiply;
+}
+console.log(arithmeticMultiplication(31, 2))*/
+let num1 = 2
+let num2 = 31
+const multiply = num1*num2
+
+let random = Math.floor(Math.random() * 10) + 1;
+/*console.log(randomNumber)*/
+let num3 = 18
+let num4 = 7
+const mod = num3 % num4
+
+
+let max = Math.max(20, 15, 7, 19)
